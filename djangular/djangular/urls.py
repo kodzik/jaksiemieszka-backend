4ae1dbc59e1e153abefc23293208ca9d678029c5
kodzik <pawlopoczta@wp.pl>
2022-05-12 18:22:12 +0200

    Cleanup.

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -17,22 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
-from graphene_django.views import GraphQLView
-from books.schema import schema
-from django.views.decorators.csrf import csrf_exempt
-
-# from jaksiemieszka.views import UserViews, ExampleView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('jaksiemieszka.urls')),
     path('account/', include('account.urls')),
-    # path('api/token/', csrf_exempt(jwt_views.TokenObtainPairView.as_view()), name='token_obtain_pair'),
-    # path('api/token/refresh/', csrf_exempt(jwt_views.TokenRefreshView.as_view()), name='token_refresh'),
-    # path('api/user/', csrf_exempt(ExampleView.as_view()), name='user'),
-
-    path("books/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
     url(r'^.*', TemplateView.as_view(template_name="index.html"), name='home'),
 ]
